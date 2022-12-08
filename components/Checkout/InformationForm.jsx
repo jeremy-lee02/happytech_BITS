c3419afa537eb660a7971810d8d058163d221a58
jeremy-lee02 <s3752975@rmit.edu.vn>
2022-12-08 11:47:17 +0700

    update

--- components/Checkout/InformationForm.jsx
+++ components/Checkout/InformationForm.jsx
@@ -6,7 +6,7 @@ const InformationForm = ({type, value, isRequired, text}) => {
     <div className='form-floating py-2'>
       <input className='form-control' 
       type={type} 
-      placeholder='' 
+      placeholder={text} 
       ref={value} 
       required = {isRequired} />
       <label className='fs-5 text'>{text}</label>
